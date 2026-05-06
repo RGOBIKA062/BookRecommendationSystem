02734225c70d17f8e10477086d83407c3df93b8f
GOBIKA R <gobikarr@gmail.com>
2026-05-06 11:19:23 +0530

    Deleted unnecessary files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,67 @@
+
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import BookRecommendation from "./components/BookRecommendation";
+import Favourites from "./components/Favourites";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Admin from "./components/Admin";
+import AdminDashboard from "./components/AdminDashboard_Premium";
+import Library from "./components/Library";
+import Chatbot from "./components/Chatbot";
+function App() {
+  const location = useLocation();
+  const showNavbar = location.pathname !== "/" && location.pathname !== "/login" && location.pathname !== "/signup";
+  return (
+    <AuthProvider>
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />        
+        {/* Protected Routes */}
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+        <Route path="/recommendation" element={
+          <ProtectedRoute>
+            <BookRecommendation />
+          </ProtectedRoute>
+        } />
+        <Route path="/favourites" element={
+          <ProtectedRoute>
+            <Favourites />
+          </ProtectedRoute>
+        } />
+        <Route path="/library" element={
+          <ProtectedRoute>
+            <Library />
+          </ProtectedRoute>
+        } />
+        <Route path="/chatbot" element={
+          <ProtectedRoute>
+            <Chatbot />
+          </ProtectedRoute>
+        } />
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/admin/legacy" element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </AuthProvider>
+  );
+}
+export default App;
